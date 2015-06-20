1865b76ed3dd33ed82d3b5c6ace5cb943baaa309
Ievgen Savochkin <nikak406@gmail.com>
2015-06-20 12:29:47 +0300

    technical commit

--- src/ce/controller/auth/AuthFilter.java
+++ src/ce/controller/auth/AuthFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(filterName = "AuthFilter", urlPatterns = "/ce/*")
+//TODO @WebFilter(filterName = "AuthFilter", urlPatterns = "/ce/*")
 public class AuthFilter implements Filter {
 
 	public void destroy() {}
